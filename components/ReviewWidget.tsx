d5419fca2e105b0bfcb72d51f133311917156a1f
Hoang4778 <hoangmd1201@gmail.com>
2024-10-28 09:41:44 +0700

    comment out the fetch widget

--- components/ReviewWidget.tsx
+++ components/ReviewWidget.tsx
@@ -4,18 +4,18 @@ import { useEffect, useRef } from "react";
 export default function ReviewWidget() {
   const widget = useRef("");
 
-  useEffect(() => {
-    async function fetchReviewWidget() {
-      const res = await fetch(
-        "https://judge.me/api/v1/widgets/product_review?shop_domain=your-daily-widget.myshopify.com&api_token=R0MI75j5Yn871ukChgfoyMHDTiI&external_id=8750293418267",
-        { cache: "no-store" }
-      );
-      const result = await res.json();
-      widget.current = result.widget;
-    }
+  // useEffect(() => {
+  //   async function fetchReviewWidget() {
+  //     const res = await fetch(
+  //       "https://judge.me/api/v1/widgets/product_review?shop_domain=your-daily-widget.myshopify.com&api_token=R0MI75j5Yn871ukChgfoyMHDTiI&external_id=8750293418267",
+  //       { cache: "no-store" }
+  //     );
+  //     const result = await res.json();
+  //     widget.current = result.widget;
+  //   }
 
-    fetchReviewWidget();
-  }, []);
+  //   fetchReviewWidget();
+  // }, []);
 
   return (
     <div
--- components/StarRating.tsx
+++ components/StarRating.tsx
@@ -4,18 +4,18 @@ import { useEffect, useRef } from "react";
 export default function StarRating() {
   const widget = useRef("");
 
-  useEffect(() => {
-    async function fetchReviewWidget() {
-      const res = await fetch(
-        "https://judge.me/api/v1/widgets/preview_badge?shop_domain=your-daily-widget.myshopify.com&api_token=R0MI75j5Yn871ukChgfoyMHDTiI&external_id=8750293418267",
-        { cache: "no-store" }
-      );
-      const result = await res.json();
-      widget.current = result.badge;
-    }
+  // useEffect(() => {
+  //   async function fetchReviewWidget() {
+  //     const res = await fetch(
+  //       "https://judge.me/api/v1/widgets/preview_badge?shop_domain=your-daily-widget.myshopify.com&api_token=R0MI75j5Yn871ukChgfoyMHDTiI&external_id=8750293418267",
+  //       { cache: "no-store" }
+  //     );
+  //     const result = await res.json();
+  //     widget.current = result.badge;
+  //   }
 
-    fetchReviewWidget();
-  }, []);
+  //   fetchReviewWidget();
+  // }, []);
 
   return (
     <div
